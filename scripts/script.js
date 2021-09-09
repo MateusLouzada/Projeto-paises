d1567665c40a37dc7d91e7de89b176d56fa110ee
Mateus Louzada <mateuslouzada01@gmail.com>
2021-09-09 10:19:20 -0300

    adicionado filtros

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
 const countrys = document.getElementById("countrys");
 const imageCountrys = document.getElementById("imageCountry");
 const detailsCountrys = document.getElementById("detailsCountry");
+var url = "https://restcountries.eu/rest/v2/all";
 
+render();
+addEventsFilters();
 
-fetch("https://restcountries.eu/rest/v2/all")
+function render() {
+    homePage();
+}
+
+function homePage() {
+fetch(url)
     .then((resp) => resp.json())
     .then(function (data) {
         data.forEach(country => {
@@ -17,6 +25,49 @@ fetch("https://restcountries.eu/rest/v2/all")
 .catch(function(error){
     console.log(error)
 });
+}
+
+function addEventsFilters() {
+    const filterDefault = document.getElementById("filterDefault");
+    const filterAfrica = document.getElementById("filterAfrica");
+    const filterAmerica = document.getElementById("filterAmerica");
+    const filterAsia = document.getElementById("filterAsia");
+    const filterEurope = document.getElementById("filterEurope");
+    const filterOceania = document.getElementById("filterOceania");
+
+
+    filterDefault.addEventListener("click", function(){
+        url = "https://restcountries.eu/rest/v2/all";
+        countrys.innerText = "";
+        render();
+    });
+
+    filterAfrica.addEventListener("click", function() { 
+        url = "https://restcountries.eu/rest/v2/region/africa";
+        countrys.innerText = "";
+        render();
+    });
+    filterAmerica.addEventListener("click", function() { 
+        url = "https://restcountries.eu/rest/v2/region/americas";
+        countrys.innerText = "";
+        render();
+    });
+    filterAsia.addEventListener("click", function() { 
+        url = "https://restcountries.eu/rest/v2/region/asia";
+        countrys.innerText = "";
+        render();
+    });
+    filterEurope.addEventListener("click", function() { 
+        url = "https://restcountries.eu/rest/v2/region/europe";
+        countrys.innerText = "";
+        render();
+    });
+    filterOceania.addEventListener("click", function() { 
+        url = "https://restcountries.eu/rest/v2/region/oceania";
+        countrys.innerText = "";
+        render();
+    });   
+}
 
 
 function insertImage(card, linkImage) {
